d217a3286a00a45a692daa287e241ca5d16b49c8
MarcusEvaristoRocha <marcusevaristo2008@gmail.com>
2025-11-14 14:26:59 -0300

    Add files via upload

--- bd/codgotemp.sql
+++ bd/codgotemp.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 19/09/2025 às 04:49
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.0.30
+-- Generation Time: 14-Nov-2025 às 18:26
+-- Versão do servidor: 10.1.13-MariaDB
+-- PHP Version: 5.6.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -18,34 +17,36 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `codgotemp`
+-- Database: `codgotemp`
 --
+CREATE DATABASE IF NOT EXISTS `codgotemp` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `codgotemp`;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `escolha`
+-- Estrutura da tabela `escolha`
 --
 
 CREATE TABLE `escolha` (
   `idForms` int(11) NOT NULL,
   `id` int(11) NOT NULL,
   `idLinguagem` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `linguagem`
+-- Estrutura da tabela `linguagem`
 --
 
 CREATE TABLE `linguagem` (
   `idLinguagem` int(11) NOT NULL,
   `nomeLinguagem` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `linguagem`
+-- Extraindo dados da tabela `linguagem`
 --
 
 INSERT INTO `linguagem` (`idLinguagem`, `nomeLinguagem`) VALUES
@@ -59,7 +60,19 @@ INSERT INTO `linguagem` (`idLinguagem`, `nomeLinguagem`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `usuario`
+-- Estrutura da tabela `professor`
+--
+
+CREATE TABLE `professor` (
+  `idProfessor` int(11) NOT NULL,
+  `nomeProfessor` varchar(50) NOT NULL,
+  `idLinguagem` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
 --
 
 CREATE TABLE `usuario` (
@@ -68,21 +81,21 @@ CREATE TABLE `usuario` (
   `email` varchar(120) NOT NULL,
   `senha` varchar(255) NOT NULL,
   `dataCadastro` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `usuario`
+-- Extraindo dados da tabela `usuario`
 --
 
 INSERT INTO `usuario` (`id`, `nomeUsuario`, `email`, `senha`, `dataCadastro`) VALUES
 (1, 'Jones', 'jones@gmail.com', '$2y$10$6GEXjHb5MtVZMgHsjYgbwORgrQ6Q.FTNfFtpKynQ3glwmKuWb72Zu', NULL);
 
 --
--- Índices para tabelas despejadas
+-- Indexes for dumped tables
 --
 
 --
--- Índices de tabela `escolha`
+-- Indexes for table `escolha`
 --
 ALTER TABLE `escolha`
   ADD PRIMARY KEY (`idForms`),
@@ -90,50 +103,64 @@ ALTER TABLE `escolha`
   ADD KEY `idLinguagem` (`idLinguagem`);
 
 --
--- Índices de tabela `linguagem`
+-- Indexes for table `linguagem`
 --
 ALTER TABLE `linguagem`
   ADD PRIMARY KEY (`idLinguagem`);
 
 --
--- Índices de tabela `usuario`
+-- Indexes for table `professor`
+--
+ALTER TABLE `professor`
+  ADD PRIMARY KEY (`idProfessor`),
+  ADD KEY `idLinguagem` (`idLinguagem`);
+
+--
+-- Indexes for table `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `escolha`
+-- AUTO_INCREMENT for table `escolha`
 --
 ALTER TABLE `escolha`
   MODIFY `idForms` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
 --
--- AUTO_INCREMENT de tabela `linguagem`
+-- AUTO_INCREMENT for table `linguagem`
 --
 ALTER TABLE `linguagem`
   MODIFY `idLinguagem` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
 --
--- AUTO_INCREMENT de tabela `usuario`
+-- AUTO_INCREMENT for table `professor`
+--
+ALTER TABLE `professor`
+  MODIFY `idProfessor` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
 
 --
--- Restrições para tabelas despejadas
---
-
---
--- Restrições para tabelas `escolha`
+-- Limitadores para a tabela `escolha`
 --
 ALTER TABLE `escolha`
   ADD CONSTRAINT `escolha_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuario` (`id`),
   ADD CONSTRAINT `escolha_ibfk_2` FOREIGN KEY (`idLinguagem`) REFERENCES `linguagem` (`idLinguagem`);
-COMMIT;
+
+--
+-- Limitadores para a tabela `professor`
+--
+ALTER TABLE `professor`
+  ADD CONSTRAINT `professor_ibfk_1` FOREIGN KEY (`idLinguagem`) REFERENCES `linguagem` (`idLinguagem`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
